f9d3788f915628c35ce13810dc1c7fa35b4e3fcc
dsnallfot <72826201+dsnallfot@users.noreply.github.com>
2025-02-09 13:33:14 +0100

    Draw forecasts at deliverAt of last determination (Dan)

--- Trio/Sources/Modules/Home/View/Chart/ChartElements/ForecastView.swift
+++ Trio/Sources/Modules/Home/View/Chart/ChartElements/ForecastView.swift
@@ -36,6 +36,7 @@ struct ForecastView: ChartContent {
     let units: GlucoseUnits
     let maxValue: Decimal
     let forecastDisplayType: ForecastDisplayType
+    let lastDeterminationDate: Date
 
     var body: some ChartContent {
         if forecastDisplayType == .lines {
@@ -46,7 +47,7 @@ struct ForecastView: ChartContent {
     }
 
     private func timeForIndex(_ index: Int32) -> Date {
-        let currentTime = Date()
+        let currentTime = lastDeterminationDate
         let timeInterval = TimeInterval(index * 300)
         return currentTime.addingTimeInterval(timeInterval)
     }
