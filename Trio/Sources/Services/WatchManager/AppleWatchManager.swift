a4ec3625766fb2a6f677bb2e925e9eabbfe54e28
dsnallfot <72826201+dsnallfot@users.noreply.github.com>
2025-02-15 16:16:17 +0100

    Early exit WatchManager initialization if no watch paired (Dan beta fix 3)

--- Trio/Sources/Services/WatchManager/AppleWatchManager.swift
+++ Trio/Sources/Services/WatchManager/AppleWatchManager.swift
@@ -45,7 +45,9 @@ final class BaseWatchManager: NSObject, WCSessionDelegate, Injectable, WatchMana
     init(resolver: Resolver) {
         super.init()
         injectServices(resolver)
-        setupWatchSession()
+        guard setupWatchSession() else {
+                     return
+                 }
 
         units = settingsManager.settings.units
         glucoseColorScheme = settingsManager.settings.glucoseColorScheme
@@ -123,23 +125,32 @@ final class BaseWatchManager: NSObject, WCSessionDelegate, Injectable, WatchMana
     }
 
     /// Sets up the WatchConnectivity session if the device supports it
-    private func setupWatchSession() {
+    private func setupWatchSession() -> Bool {
         if WCSession.isSupported() {
             let session = WCSession.default
             session.delegate = self
             session.activate()
             self.session = session
-
+            
             debug(.watchManager, "📱 Phone session setup - isPaired: \(session.isPaired)")
+            
+            guard session.isPaired else {
+                debug(.watchManager, "⌚️❌ No Watch is paired")
+                // return here to end any further initialization of Apple Watch Manager
+                return false
+            }
+            return true
         } else {
             debug(.watchManager, "📱 WCSession is not supported on this device")
+            // return here to end any further initialization of Apple Watch Manager
+            return false
         }
     }
-
+    
     /// Attempts to reestablish the Watch connection if it becomes unreachable
     private func retryConnection() {
         guard let session = session else { return }
-
+        
         if !session.isReachable {
             debug(.watchManager, "📱 Attempting to reactivate session...")
             session.activate()
