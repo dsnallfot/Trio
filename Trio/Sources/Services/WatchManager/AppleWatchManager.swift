36560bed7b2ae4b608cbd4db0b755cc880a24bbf
dsnallfot <72826201+dsnallfot@users.noreply.github.com>
2025-02-16 22:51:34 +0100

    Revert "Early exit WatchManager initialization if no watch paired"

--- Trio/Sources/Services/WatchManager/AppleWatchManager.swift
+++ Trio/Sources/Services/WatchManager/AppleWatchManager.swift
@@ -45,9 +45,7 @@ final class BaseWatchManager: NSObject, WCSessionDelegate, Injectable, WatchMana
     init(resolver: Resolver) {
         super.init()
         injectServices(resolver)
-        guard setupWatchSession() else {
-            return
-        }
+        setupWatchSession()
 
         units = settingsManager.settings.units
         glucoseColorScheme = settingsManager.settings.glucoseColorScheme
@@ -125,7 +123,7 @@ final class BaseWatchManager: NSObject, WCSessionDelegate, Injectable, WatchMana
     }
 
     /// Sets up the WatchConnectivity session if the device supports it
-    private func setupWatchSession() -> Bool {
+    private func setupWatchSession() {
         if WCSession.isSupported() {
             let session = WCSession.default
             session.delegate = self
@@ -133,17 +131,8 @@ final class BaseWatchManager: NSObject, WCSessionDelegate, Injectable, WatchMana
             self.session = session
 
             debug(.watchManager, "📱 Phone session setup - isPaired: \(session.isPaired)")
-
-            guard session.isPaired else {
-                debug(.watchManager, "⌚️❌ No Watch is paired")
-                // return here to end any further initialization of Apple Watch Manager
-                return false
-            }
-            return true
         } else {
             debug(.watchManager, "📱 WCSession is not supported on this device")
-            // return here to end any further initialization of Apple Watch Manager
-            return false
         }
     }
 
